bfeb63cbd63a544e3127ba02230d42236f0ae746
Lena Vadakkel <lena.vadakkel@ubs.com>
2023-11-29 15:34:51 +0100

    - code clean up -  (most of the forces are gone, found an other way)

---
+++ Notes/ViewModifier/ErrorMessageText.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorMessageText.swift
+//  Notes
+//
+//  Created by Lena Vadakkel on 24.11.23.
+//
+
+import Foundation
---
+++ Notes/ViewModifier/ViewExtensionForModifiers.swift
@@ -0,0 +1,8 @@
+//
+//  ViewExtensionForModifiers.swift
+//  Notes
+//
+//  Created by Lena Vadakkel on 24.11.23.
+//
+
+import Foundation
